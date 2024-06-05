eda41f16e1947ef5f781fd75b5dc9df1211d1311
JJK <chang1381023@gmail.com>
2024-06-05 23:06:42 +0800

    liquibase upload

--- liquibase-demo/src/main/resources/db/changelog/sql/insertdata_viewpractice.sql
+++ liquibase-demo/src/main/resources/db/changelog/sql/insertdata_viewpractice.sql
@@ -2,7 +2,3 @@ INSERT INTO viewpractice (category, map_id) VALUES ('Category1', 1);
 INSERT INTO viewpractice (category, map_id) VALUES ('Category2', 2);
 INSERT INTO viewpractice (category, map_id) VALUES ('Category3', 1);
 INSERT INTO viewpractice (category, map_id) VALUES ('Category4', 1);
-
-
-INSERT INTO viewpractice_mappid (category) VALUES ('Category1');
-INSERT INTO viewpractice_mappid (category) VALUES ('Category2');
