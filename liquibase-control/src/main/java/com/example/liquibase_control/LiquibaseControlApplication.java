5fa99a725f81a92526585d92a3e4164cfca5fdb5
JJK <chang1381023@gmail.com>
2024-06-10 19:23:27 +0800

    liquibase

---
+++ liquibase-control/src/main/java/com/example/liquibase_control/LiquibaseControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibase_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseControlApplication.class, args);
+	}
+
+}
---
+++ liquibase-control/src/test/java/com/example/liquibase_control/LiquibaseControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquibase_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
